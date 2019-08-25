d3abfc42b0c7596ba55515ffcd5d92e7dcbe7892
taehyeong77@naver.com <taehyeong77@naver.com>
2019-08-25 21:05:32 +0900

    commit

---
+++ Project1/Project1.cpp
@@ -0,0 +1,63 @@
+﻿// Project1.cpp : 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include "Project1.h"
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	if (uMsg == WM_DESTROY) {
+		PostQuitMessage(0);
+	}
+	else if (uMsg == WM_LBUTTONDOWN) {
+		// FindWindow(L"Notepad", L"제목 없음 - 메모장");
+		HWND h_find_wnd = FindWindow(L"Notepad", NULL);
+
+		if (NULL != h_find_wnd) {
+			HWND h_edit_wnd = FindWindowEx(h_find_wnd, NULL, L"Edit", NULL);
+
+			if (NULL != h_edit_wnd) {
+				wchar_t str[256];
+				SendMessage(h_edit_wnd, WM_GETTEXT, 256, (LPARAM)str);
+				SendMessage(h_edit_wnd, WM_SETTEXT, 0, (LPARAM)L"안녕하세요");
+				MessageBox(hWnd, str, L"에디트에 적힌 내용", MB_OK);
+			}
+		}
+	}
+		
+
+	return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	WNDCLASS wc;
+
+	wchar_t my_class_name[] = L"tipssoft";
+
+	wc.cbClsExtra = NULL;
+	wc.cbWndExtra = NULL;
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hInstance = hInstance;
+	wc.lpfnWndProc = WndProc;
+	wc.lpszClassName = my_class_name;
+	wc.lpszMenuName = NULL;
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+
+	RegisterClass(&wc);
+
+	HWND hWnd = CreateWindow(my_class_name, L"www.tipssoft.com",
+		WS_OVERLAPPEDWINDOW, 100, 90, 400, 350, NULL, NULL, hInstance, NULL);
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return msg.wParam;
+}
